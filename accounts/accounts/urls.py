63e09fcf5de91708e378c702b3770e28f261a035
PC-20180507SJFP\Administrator <hxy0229@github.com>
2018-08-27 14:16:35 +0800

    signup html without pop out windows created

--- accounts/accounts/urls.py
+++ accounts/accounts/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path('signup/', include('signup.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]

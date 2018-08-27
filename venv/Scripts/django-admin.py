4e8362d2199d3a1519bcaa99a4ff6a7d58db7e8c
PC-20180507SJFP\Administrator <hxy0229@github.com>
2018-08-27 10:29:04 +0800

    Original About-me Page

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\New HXY documents\Singtel Internship-2018 Summer\DjangoProject1\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

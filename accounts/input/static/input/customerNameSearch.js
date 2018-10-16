ec3e3144b2a65253fed7fa7506e3309f5bc48aa0
PC-20180507SJFP\Administrator <hxy0229@github.com>
2018-10-16 14:39:31 +0800

    forms.py starts to work

--- accounts/input/static/input/customerNameSearch.js
+++ accounts/input/static/input/customerNameSearch.js
@@ -4,21 +4,13 @@ var searchByCustomerNameModal = document.getElementById("searchByCustomerNameMod
 var searchByCustomerNameBtn = document.getElementById("searchByCustomerNameBtn");
 
 // Get the <span> element that closes the modal
-var closeModalBtn = document.getElementById("closeModalBtn");
-
 // When the user clicks on the button, open the modal
 searchByCustomerNameBtn.onclick = function(){
     searchByCustomerNameModal.style.display = "block";
 }
 
+var closeModalBtn = document.getElementByClassName("closeModalBtn");
 // When the user clicks on <span> (x), close the modal
 closeModalBtn.onclick = function(){
     searchByCustomerNameModal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
-/*window.onclick = function(event){
-    if (event.target == modal) {
-        searchByCustomerNameModal.style.display = "none";
-    }
-}*/
